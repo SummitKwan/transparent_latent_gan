46d4db83a46a66f643b710f9b98364912471af1f
ubuntu <summitkwan@hotmail.com>
2018-09-25 23:56:36 +0000

    generate 128*128 cropped image for celebA

--- src/model/feature_finding_cnn.py
+++ src/model/feature_finding_cnn.py
@@ -16,11 +16,32 @@ df_attr = pd.read_csv(path_celeba_att, sep='\s+', header=1, index_col=0)
 df_attr.head(3)
 
 # load figures
-image_filenames = glob.glob(os.path.join(path_celeba_img, '*.jpg'))
-image_filenames.sort()
+# image_filenames = glob.glob(os.path.join(path_celeba_img, '*.jpg'))
+# image_filenames.sort()
 
-assert df_attr.shape[0] == len(list_img), 'images number does not match attribute table'
+img_names = os.listdir(path_celeba_img)
+img_names = [img_name for img_name in img_names if img_name[-4:]=='.jpg']
+img_names.sort()
+
+assert df_attr.shape[0] == len(img_names), 'images number does not match attribute table'
 num_img = df_attr.shape[0]
+
+
+##
+
+
+def get_image_feature_pair(img_idx):
+    img_name = img_names[img_idx]
+    img = np.asarray(PIL.Image.open(os.path.join(path_celeba_img, img_name)))
+    plt.imshow(img)
+    plt.show()
+    print
+    # print(df_attr.ix[img_idx])
+    print()
+
+
+i_rand = np.random.randint(num_img)
+get_image_feature_pair(i_rand)
 
 ##
 pd.set_option('display.max_columns', 50)
