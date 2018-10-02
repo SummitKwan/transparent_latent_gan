7c63211653737cfbb008e78e60ca927906799ca5
ubuntu <summitkwan@hotmail.com>
2018-10-02 06:32:41 +0000

    decorrelate feature axis through vector operation

--- src/tf_gan/feature_axis.py
+++ src/tf_gan/feature_axis.py
@@ -40,8 +40,75 @@ def normalize_feature_axis(feature_slope):
     :return: same shape of input
     """
 
-    feature_direction = feature_slope / np.sqrt(np.sum(feature_slope**2, axis=0, keepdims=True))
+    feature_direction = feature_slope / np.linalg.norm(feature_slope, ord=2, axis=0, keepdims=True)
     return feature_direction
+
+
+def decorrelate_feature_axis(feature_axis_to_decorrelate, feature_axis_base, yn_base_othogonalized=False):
+    """
+    make feature_axis_to_decorrelate orthogonal to feature_axis_base
+
+    :param feature_axis_to_decorrelate: features axes to decorrerelate, shape = (num_dim, num_feature_0)
+    :param feature_axis_base: features axes to decorrerelate, shape = (num_dim, num_feature_1))
+    :param yn_base_othogonalized: True/False whether the feature_axis_base is already othogonalized
+    :return: feature_axis_decorrelated, shape = shape = (num_dim, num_feature_0)
+    """
+
+    # make sure this funciton works to 1D vector
+    if len(feature_axis_to_decorrelate.shape) == 0:
+        yn_single_vector_in = True
+        feature_axis_to_decorrelate = feature_axis_to_decorrelate[:, None]
+    else:
+        yn_single_vector_in = False
+
+    # if already othogonalized, skip this step
+    if yn_base_othogonalized:
+        feature_axis_base_orthononal = orthogonalize_vectors(feature_axis_base)
+    else:
+        feature_axis_base_orthononal = feature_axis_base
+
+    # orthogonalize every vector
+    feature_axis_decorrelated = feature_axis_to_decorrelate + 0
+    num_dim, num_feature_0 = feature_axis_to_decorrelate.shape
+    num_dim, num_feature_1 = feature_axis_base_orthononal.shape
+    for i in range(num_feature_0):
+        for j in range(num_feature_1):
+            feature_axis_decorrelated[:, i] = orthogonalize_one_vector(feature_axis_decorrelated[:, i],
+                                                                       feature_axis_base_orthononal[:, j])
+
+    # make sure this funciton works to 1D vector
+    if yn_single_vector_in:
+        result = feature_axis_decorrelated[:, 0]
+    else:
+        result = feature_axis_decorrelated
+
+    return result
+
+
+def orthogonalize_one_vector(vector, vector_base):
+    """
+    tool function, adjust vector so that it is orthogonal to vector_base (i.e., vector - its_projection_on_vector_base )
+
+    :param vector0: 1D array
+    :param vector1: 1D array
+    :return: adjusted vector1
+    """
+    return vector - np.dot(vector, vector_base)/np.dot(vector_base, vector_base) * vector_base
+
+
+def orthogonalize_vectors(vectors):
+    """
+    tool function, adjust vectors so that they are orthogonal to each other, takes O(num_vector^2) time
+
+    :param vectors: vectors, shape = (num_dimension, num_vector)
+    :return: orthorgonal vectors, shape = (num_dimension, num_vector)
+    """
+    vectors_orthogonal = vectors + 0
+    num_dimension, num_vector = vectors.shape
+    for i in range(num_vector):
+        for j in range(i):
+            vectors_orthogonal[:, i] = orthogonalize_one_vector(vectors_orthogonal[:, i], vectors_orthogonal[:, j])
+    return vectors_orthogonal
 
 
 
---
+++ tests/test_feature_axis_orthogonalize.py
@@ -0,0 +1,26 @@
+""" test script """
+
+import importlib
+import numpy as np
+import src.tf_gan.feature_axis as feature_axis
+
+importlib.reload(feature_axis)
+
+vectors = np.random.rand(10, 4)
+
+print(np.sum(vectors**2, axis=0))
+
+vectors_normalized = feature_axis.normalize_feature_axis(vectors)
+
+print(np.sum(vectors_normalized**2, axis=0))
+
+print(feature_axis.orthogonalize_one_vector(np.array([1, 0, 0 ]), np.array([1,1,1])))
+
+print(vectors_normalized)
+
+vectors_orthogonal = feature_axis.orthogonalize_vectors(vectors_normalized)
+
+print(np.dot(vectors_normalized[:, -2], vectors_normalized[:, -1]))
+print(np.dot(vectors_orthogonal[:, -2], vectors_orthogonal[:, -1]))
+
+
